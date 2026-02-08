9f3493a29fd045bbe743323cf5ee1401142ca966
Joshua-Deering <jidjosh2006@icloud.com>
2026-02-08 00:17:42 -0500

    Initial edits

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,21 +1,14 @@
 import { FC } from "react";
 
-import { Box, Flex, Heading, Text, Image, Button, IconButton } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
 
 import { configs, Content, MarkdownFile, useContent } from "shared/content/Content";
-import { Blog } from "pages/about/blog/Blog";
 import { Education } from "pages/about/education/Education";
 import { Experience } from "pages/about/experience/Experience";
 import { Skills } from "pages/about/skills/Skills";
-import { VolumeIcon } from "utils/Icons";
 
 export const About: FC = () => {
     const content = useContent(MarkdownFile.About);
-
-    const onPlay = () => {
-        const audio = new Audio(configs.common.audioFile);
-        audio.play();
-    };
 
     return (
         <Box>
@@ -29,23 +22,6 @@ export const About: FC = () => {
                 </Box>
                 <Box flex="0.85">
                     <Heading data-aos="fade-down">{configs.common.name}</Heading>
-                    <Flex alignItems="center">
-                        <Text fontWeight="bold" opacity="0.5" data-aos="fade" data-aos-delay="200">
-                            {configs.common.pronunciation}
-                        </Text>
-
-                        <Button
-                            size="xs"
-                            aria-label="pronunciation button"
-                            as={IconButton}
-                            variant="icon"
-                            fontSize="md"
-                            icon={<VolumeIcon />}
-                            onClick={onPlay}
-                            data-aos="fade"
-                            data-aos-delay="400"
-                        />
-                    </Flex>
                     <Box pt="4" data-aos="fade-up" data-aos-delay="400">
                         <Content fontSize="lg">{content.about}</Content>
                     </Box>
@@ -66,9 +42,6 @@ export const About: FC = () => {
             </Flex>
             <Box pt="16">
                 <Skills />
-            </Box>
-            <Box pt="16">
-                <Blog />
             </Box>
         </Box>
     );
