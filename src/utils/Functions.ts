6abb7e7631c58e2c52d3cbd04790fab22573332e
Joshua Deering <jidjosh2006@icloud.com>
2026-08-04 16:40:31 -0400

    fix resume link
    Refactor open function to create and click an anchor element for better handling of link opening.

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -1,6 +1,14 @@
 import { configs } from "shared/content/Content";
 
-export const open = (link: string) => window.open(link, "_blank");
+export const open = (link: string) => {
+  const anchor = document.createElement("a");
+  anchor.href = link;
+  anchor.target = "_blank";
+  anchor.rel = "noreferrer"; // This blocks the referrer header Google hates
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+};
 
 export const onResumeOpen = () => {
     open(configs.common.resume);
